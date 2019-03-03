815d5b6ff393e98f9321800a2c339fe99c3bfafa
Sushil <shinde.sushil@gmail.com>
2019-03-03 12:13:09 +0530

    deploy on heroku

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import initializeDb from './db';
 import middleware from './middleware';
 import api from './api';
 import config from './config.json';
-
+//http://localhost:8080/api/
 let app = express();
 app.server = http.createServer(app);
 
